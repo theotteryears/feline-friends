f9970f10e20e48232c19867b57704b25ac854e72
Chris Collis <theotteryears@me.com>
2024-05-07 19:20:58 +0100

    Add role to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_124244) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_180320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_124244) do
     t.string "last_name"
     t.string "address"
     t.text "details"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
