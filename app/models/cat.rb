6f7dab8af24ef822fd6fa87dcc16b15d379fb181
Chris Collis <theotteryears@me.com>
2024-05-07 19:26:50 +0100

    Add dependant destroy to Cat model for matches

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,7 +1,7 @@
 class Cat < ApplicationRecord
   belongs_to :user
-  has_many :ratings
-  has_many :matches
+  has_many :ratings, dependent: :destroy
+  has_many :matches, dependent: :destroy
   validates :name, presence: true
   validates :details, presence: true
 end
