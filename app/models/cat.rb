5b842f6e1e26c13a7931614baa387c8511e4f5ad
amy hastings <amyh364@outlook.com>
2024-05-08 14:40:28 +0100

    img_1 presence validation and iteration to check presence of photos for display

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,6 +3,7 @@ class Cat < ApplicationRecord
   has_many :matches, dependent: :destroy
   validates :name, presence: true
   validates :details, presence: true
+  validates :img_1, presence: true
   has_one_attached :img_1
   has_one_attached :img_2
   has_one_attached :img_3
