b27449051e4ae13b427d2aa3028fa5c1f396583c
amy hastings <amyh364@outlook.com>
2024-05-08 14:21:33 +0100

    multiple img upload done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_093105) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_08_131036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_093105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_1"
+    t.string "img_2"
+    t.string "img_3"
     t.index ["user_id"], name: "index_cats_on_user_id"
   end
 
