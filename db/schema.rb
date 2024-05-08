d1425294a5e9a5c941cf0ce017db4a915e86e531
Chris Collis <theotteryears@me.com>
2024-05-08 14:43:23 +0100

    Rename users table address to city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_131036) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_08_134017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,7 +74,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_131036) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "address"
+    t.string "city"
     t.text "details"
     t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
