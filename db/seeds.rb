bb0bfa391775b36b5593b9fdb9cfbb3e2bbe3a1a
Chris Collis <theotteryears@me.com>
2024-05-08 11:53:22 +0100

    Added logic to deal with the different log ins

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ Match.destroy_all
 
 puts 'Creating users'
 
-User.create!(email: "sitter@test.com", password: "123456", first_name: "Cat", last_name: "Sitter", details: "Hi, I am a cat sitter and I love cats")
-User.create!(email: "owner@test.com", password: "123456", first_name: "Cat", last_name: "Owner", details: "Hi, I am a cat owner and I really need to find a cat sitter")
+User.create!(role: 0, email: "sitter@test.com", password: "123456", first_name: "Cat", last_name: "Sitter", details: "Hi, I am a cat sitter and I love cats")
+User.create!(role: 1, email: "owner@test.com", password: "123456", first_name: "Cat", last_name: "Owner", details: "Hi, I am a cat owner and I really need to find a cat sitter")
 
 puts 'Creating cats'
 
