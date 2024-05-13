d10ec235a293a2b3a5f19c44f870c70c1c4e30d6
Soraya Dos Santos <dossantos.soraya@gmail.com>
2024-05-13 12:57:37 +0200

    rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_09_132511) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_074200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_132511) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "ratings", force: :cascade do |t|
+    t.integer "rating"
+    t.bigint "cat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cat_id"], name: "index_ratings_on_cat_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "breed"
     t.datetime "created_at", null: false
@@ -127,4 +135,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_132511) do
   add_foreign_key "matches", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "ratings", "cats"
 end
